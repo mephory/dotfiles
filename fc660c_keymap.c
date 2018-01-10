d95b288878db87452599210520df773f0e71631b
mephory <mephory@mephory.com>
2018-01-10 21:05:06 +0100

    new fc660c keymap

--- fc660c_keymap.c
+++ fc660c_keymap.c
@@ -3,10 +3,21 @@
 #include "hook.h"
 #include "fc660c.h"
 
+#define MACRO_TMUX_LAST_SESSION   1
+#define MACRO_TYPE_EMAIL 2
+#define MACRO_TMUX_NEXT_WINDOW 3
+#define MACRO_TMUX_PREV_WINDOW 4
+#define MACRO_TMUX_LAST_WINDOW 5
+#define MACRO_TMUX_CREATE_WINDOW 6
+
 #define AC_L1       ACTION_LAYER_MOMENTARY(1)
 #define AC_LS_2     ACTION_LAYER_MODS(2, MOD_LSFT)
-#define AC_M1       ACTION_MACRO(1)
+#define AC_M_TM_LS  ACTION_MACRO(1)
 #define AC_M2       ACTION_MACRO(2)
+#define AC_M_TM_NW  ACTION_MACRO(3)
+#define AC_M_TM_PW  ACTION_MACRO(4)
+#define AC_M_TM_LW  ACTION_MACRO(5)
+#define AC_M_TM_CW  ACTION_MACRO(6)
 
 #ifdef KEYMAP_SECTION_ENABLE
 const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
@@ -23,8 +34,8 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     [1] = KMAP(
         GRV,   F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,TRNS,     TRNS,
         TRNS,MS_BTN2,MS_UP,M2,TRNS,TRNS,TRNS,PGUP,HOME,PGDN,PSCR,TRNS,TRNS,TRNS,     TRNS,
-        TRNS,MS_LEFT,MS_DOWN,MS_RIGHT,MS_BTN1,M1,LEFT,DOWN,UP,RIGHT,INSERT,DELETE,     TRNS,
-        TRNS,TRNS,TRNS,ESC,TRNS,TRNS,END,ACL0,ACL1,ACL2,TRNS,          TRNS,TRNS,
+        TRNS,M_TM_LW,M_TM_PW,M_TM_NW,MS_BTN1,M_TM_LS,LEFT,DOWN,UP,RIGHT,INSERT,DELETE,     TRNS,
+        TRNS,TRNS,TRNS,M_TM_CW,TRNS,TRNS,END,ACL0,ACL1,ACL2,TRNS,          TRNS,TRNS,
         TRNS,TRNS,TRNS,          ACL0,                    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS
     ),
     [2] = KMAP(
@@ -34,13 +45,35 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,TRNS,
         TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS
     ),
+    [3] = KMAP(
+        GRV,   F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,TRNS,     TRNS,
+        TRNS,MS_BTN2,MS_UP,M2,TRNS,TRNS,TRNS,PGUP,HOME,PGDN,PSCR,TRNS,TRNS,TRNS,     TRNS,
+        TRNS,MS_LEFT,MS_DOWN,MS_RIGHT,MS_BTN1,M_TM_LS,LEFT,DOWN,UP,RIGHT,INSERT,DELETE,     TRNS,
+        TRNS,TRNS,TRNS,ESC,TRNS,TRNS,END,ACL0,ACL1,ACL2,TRNS,          TRNS,TRNS,
+        TRNS,TRNS,TRNS,          ACL0,                    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS
+    ),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        switch(id) {
+        case MACRO_TMUX_LAST_SESSION:
+            return MACRO(I(0), D(LCTL), T(A), U(LCTL), D(LSFT), T(L), U(LSFT), END);
+        case MACRO_TYPE_EMAIL:
+            return MACRO(I(0), T(M), T(E), T(P), T(H), T(O), T(R), T(Y), D(LSFT), T(2), U(LSFT), T(M), T(E), T(P), T(H), T(O), T(R), T(Y), T(DOT), T(C), T(O), T(M), END);
+        case MACRO_TMUX_PREV_WINDOW:
+            return MACRO(I(0), D(LCTL), T(A), U(LCTL), T(P), END);
+        case MACRO_TMUX_NEXT_WINDOW:
+            return MACRO(I(0), D(LCTL), T(A), U(LCTL), T(N), END);
+        case MACRO_TMUX_LAST_WINDOW:
+            return MACRO(I(0), D(LCTL), T(A), T(A), U(LCTL), END);
+        case MACRO_TMUX_CREATE_WINDOW:
+            return MACRO(I(0), D(LCTL), T(A), U(LCTL), T(C), END);
+        }
+    }
+    return MACRO_NONE;
     if (id == 1 && record->event.pressed) {
-        return MACRO(I(0), D(LCTL), T(A), U(LCTL), D(LSFT), T(L), U(LSFT), END);
     } else if (id == 2 && record->event.pressed) {
-        return MACRO(I(0), T(M), T(E), T(P), T(H), T(O), T(R), T(Y), D(LSFT), T(2), U(LSFT), T(M), T(E), T(P), T(H), T(O), T(R), T(Y), T(DOT), T(C), T(O), T(M), END);
     } else {
         return MACRO_NONE;
     }
